03198df7b5b8e0788d15c63efaba652d44b86b0a
cartacode <cartacode@outlook.com>
2018-10-23 04:13:05 -0400

    fix send message funtionality

--- popup.js
+++ popup.js
@@ -1,29 +1,32 @@
-// var s = document.createElement ("script");
-// s.src = chrome.extension.getURL ("js/jquery3.min.js");
-// s.async = false;
-// document.documentElement.appendChild (s);
-
 var port = chrome.extension.connect({
     name: "Sample Communication"
 });
 
 port.onMessage.addListener(function(msg) {
-    console.log('pop es:', msg)
+    console.log('pop received message:', msg)
 });
 
 var playButton = document.getElementById('play');
 var pauseButton = document.getElementById('pause');
 
-
 playButton.onclick = function(element) {
 	console.log("Connected .....");
-	// check selection text can be converted into digit
 	port.postMessage("play_");
 }
 
 pauseButton.onclick = function(element) {
-	console.log('ok')
 	console.log("Connected .....");
-	// check selection text can be converted into digit
 	port.postMessage("pause_");
 }
+
+var player = document.getElementById("play");
+player.addEventListener("click", function () {
+  console.log("it's click time");
+});
+player.addEventListener("play", function () {
+  console.log("it's go time");
+});
+
+player.addEventListener("pause", function () {
+  console.log("it's pause time");
+});
